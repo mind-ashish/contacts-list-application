16f5a8efbffc71d162f8c7ab0e0763f6f8f6f39d
Aashish <aashish01official@gmail.com>
2019-07-03 01:18:33 +0530

    adds server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+const express=require('express');
+const port=8000;  
+const app=express();
+
+const path=require('path');
+
+//setting template/view engine
+app.set('view engine','ejs');
+
+//setting view folder
+app.set('views',path.join(__dirname,'views'));
+
+//setting assets folder for static file using middleware express.static
+app.use(express.static('assets'));
+
+var arr=[];
+app.use(express.urlencoded());
+
+app.get('/',function(req,res){
+    return res.render('index',{
+        title:"Contact List",
+        contact_list:arr
+
+    });
+});
+
+app.post('/',function(req,res){
+    arr.push(req.body);   
+    return res.redirect('back');  //to redirect back to home page after this.
+});
+
+
+app.get('/delete-contact/',function(req,res){
+    // console.log(req.query.name);
+    // console.log(req.query.phone);
+
+    for(var i=0;i<arr.length;i++){
+        if(arr[i].phone==req.query.phone){
+            arr.splice(i,1);
+            break;
+        }
+    }
+    return res.redirect('back');
+
+});
+
+app.listen(port,function(err){
+console.log('server is up and running at port',port);
+});
